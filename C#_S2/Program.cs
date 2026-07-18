ee46cc46bbbb27ebbd54d9997dbfe15bbbdf12e4
EL3RABAWY <yshm2@sci.suezuni.edu.eg>
2026-07-18 18:01:58 +0300

    question2

--- C#_S2/Program.cs
+++ C#_S2/Program.cs
@@ -3,7 +3,7 @@
     internal class Program
 
     {
-        #region Question 1
+           #region Question 1
         //public class Book
         //{
         //    public string? Title;
@@ -21,11 +21,14 @@
         //    Book book = new Book { Title = "C# Tutorial", Pages = 209 };
         //    object obj = book;
         //    Console.WriteLine(obj);
-           #endregion
-            Console.WriteLine(book.ToString());
-            Console.WriteLine(book.Equals(book));
-            Console.WriteLine(book.GetHashCode());
-            Console.WriteLine(book.GetType());
+            #endregion
+
+            #region question 2
+            //Console.WriteLine(book.ToString());
+            //Console.WriteLine(book.Equals(book));
+            //Console.WriteLine(book.GetHashCode());
+            //Console.WriteLine(book.GetType());
+            #endregion
 
             //int pages = "464"; compile error
             string pages = "464";
@@ -33,96 +36,96 @@
             try
             {
                 int zero = 0;
-                int result = 10 / zero;
-            }
+        int result = 10 / zero;
+    }
             catch (DivideByZeroException)
             {
                 Console.WriteLine(" ERROR : Cannot divide by zero ");
             }
             finally
             {
-                Console.WriteLine("Done");
-            }
+    Console.WriteLine("Done");
+}
 
-            int No_of_pages = 300;
-            double pagesAsDouble = No_of_pages;
-            Console.WriteLine(pagesAsDouble);
+int No_of_pages = 300;
+double pagesAsDouble = No_of_pages;
+Console.WriteLine(pagesAsDouble);
 
-            Console.WriteLine("_______________________________________________");
+Console.WriteLine("_______________________________________________");
 
-            double price = 50.99;
-            int roundedPrice = (int)price;
-            Console.WriteLine(roundedPrice);
+double price = 50.99;
+int roundedPrice = (int)price;
+Console.WriteLine(roundedPrice);
 
-            Console.WriteLine("_______________________________________________");
+Console.WriteLine("_______________________________________________");
 
-            string pagesText = "464";
-            int no_pages = Convert.ToInt32(pagesText);
-            Console.WriteLine(no_pages);
+string pagesText = "464";
+int no_pages = Convert.ToInt32(pagesText);
+Console.WriteLine(no_pages);
 
-            Console.WriteLine("_______________________________________________");
+Console.WriteLine("_______________________________________________");
 
-            string yearText = "2026";
-            int year = int.Parse(yearText);
-            Console.WriteLine(year);
+string yearText = "2026";
+int year = int.Parse(yearText);
+Console.WriteLine(year);
 
-            string badText = "FGH";
+string badText = "FGH";
 
-            if (int.TryParse(badText, out int Result))
-            {
-                Console.WriteLine(Result);
-            }
-            else
-            {
-                Console.WriteLine("Invalid number");
-            }
+if (int.TryParse(badText, out int Result))
+{
+    Console.WriteLine(Result);
+}
+else
+{
+    Console.WriteLine("Invalid number");
+}
 
-            Console.WriteLine("_______________________________________________");
+Console.WriteLine("_______________________________________________");
 
-            int pagesNo = 464;
-            string pagestext = pages.ToString();
-            Console.WriteLine(pagesText);
-            Console.WriteLine(pagesText.GetType());
+int pagesNo = 464;
+string pagestext = pages.ToString();
+Console.WriteLine(pagesText);
+Console.WriteLine(pagesText.GetType());
 
-            Console.WriteLine("_______________________________________________");
+Console.WriteLine("_______________________________________________");
 
-            int copies = 100;
-            object boxed = copies;
-            int unboxed = (int)boxed;
-            Console.WriteLine(boxed);
-            Console.WriteLine(unboxed);
+int copies = 100;
+object boxed = copies;
+int unboxed = (int)boxed;
+Console.WriteLine(boxed);
+Console.WriteLine(unboxed);
 
-            Console.WriteLine("_______________________________________________");
+Console.WriteLine("_______________________________________________");
 
-            int? year1 = null;
-            Console.WriteLine(year1.HasValue);
+int? year1 = null;
+Console.WriteLine(year1.HasValue);
 
-            year1 = 2023;
-            Console.WriteLine(year1.Value);
+year1 = 2023;
+Console.WriteLine(year1.Value);
 
-            Console.WriteLine("_______________________________________________");
+Console.WriteLine("_______________________________________________");
 
-            string? reviewer = null;
-            Console.WriteLine(reviewer is null);
+string? reviewer = null;
+Console.WriteLine(reviewer is null);
 
-            Console.WriteLine("_______________________________________________");
+Console.WriteLine("_______________________________________________");
 
-            Book? book1 = null;
-            string? title = book1?.Title;
-            Console.WriteLine(title);
+Book? book1 = null;
+string? title = book1?.Title;
+Console.WriteLine(title);
 
-            Console.WriteLine("_______________________________________________");
+Console.WriteLine("_______________________________________________");
 
-            Console.WriteLine(title ?? "None");
+Console.WriteLine(title ?? "None");
 
-            title ??= "None";
-            Console.WriteLine(title);
+title ??= "None";
+Console.WriteLine(title);
 
-            Console.WriteLine("_______________________________________________");
+Console.WriteLine("_______________________________________________");
 
-            string? name = "Ali";
-            string confirmedName = name!;
-            Console.WriteLine(confirmedName);
+string? name = "Ali";
+string confirmedName = name!;
+Console.WriteLine(confirmedName);
 
         }
     }
